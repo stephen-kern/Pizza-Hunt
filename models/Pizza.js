7d955ca02af47c530372f8c7dce1e74a9b7dd46a
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-11-07 23:30:57 -0600

    created comment model and schema and allowed the use of virtuals and getters

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,27 +1,48 @@
 // import the schema constructor and the model function from mongoose
 const { Schema, model } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
 
 // Create the schema and define the fields (for clarity)
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
